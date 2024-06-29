7e6f57cb817beb52096f97e3238568d9745e6268
Kevin Konczak <konczakkevin@gmail.com>
2024-06-29 19:03:31 +0900

    test to see if live DB works

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,9 @@
 import type { MetaFunction } from "@remix-run/node";
 import { Box } from "@mantine/core";
+import { PrismaClient } from "@prisma/client";
+import { useLoaderData } from "@remix-run/react";
+
+const prisma = new PrismaClient();
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,7 +12,14 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export async function loader() {
+  const users = await prisma.user.findMany();
+  return { users };
+}
+
 export default function Index() {
+  const { users } = useLoaderData<typeof loader>();
+  console.log(users);
   return (
     <Box>
       <p>Index</p>

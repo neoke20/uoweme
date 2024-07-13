d666d5ee352e98978c808432fc5ec69c2c82958c
Kevin Konczak <konczakkevin@gmail.com>
2024-07-13 21:03:47 +0900

    updating db to check new deploy on Netlify

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,9 +1,9 @@
 import type { MetaFunction } from "@remix-run/node";
 import { Box } from "@mantine/core";
-// import { PrismaClient } from "@prisma/client";
-// import { useLoaderData } from "@remix-run/react";
+import { useLoaderData } from "@remix-run/react";
+import { PrismaClient } from "@prisma/client";
 
-// const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
 export const meta: MetaFunction = () => {
   return [
@@ -12,14 +12,14 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-// export async function loader() {
-//   const users = await prisma.user.findMany();
-//   return { users };
-// }
+export async function loader() {
+  const users = await prisma.user.findMany();
+  return { users };
+}
 
 export default function Index() {
-  // const { users } = useLoaderData<typeof loader>();
-  // console.log(users);
+  const { users } = useLoaderData<typeof loader>();
+  console.log(users);
   return (
     <Box>
       <p>Index</p>

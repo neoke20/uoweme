94d916f61ed95a251f6ce0654620d27464a1a0ee
Kevin Konczak <konczakkevin@gmail.com>
2024-07-14 18:19:00 +0900

    styling the login page

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,7 +1,50 @@
+import {
+  Text,
+  Anchor,
+  Button,
+  Checkbox,
+  Container,
+  Group,
+  Paper,
+  PasswordInput,
+  TextInput,
+  Title,
+  Stack,
+} from "@mantine/core";
+import classes from "~/style/AuthenticationTitle.module.css";
+
 export default function Login() {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Stack h={"100vh"} bg={"charcoal.6"} justify={"center"}>
+      <Container>
+        <Title ta="center" className={classes.title}>
+          Welcome back!
+        </Title>
+        <Text c="white" size="sm" ta="center" mt={5}>
+          Do not have an account yet?{" "}
+          <Anchor c="white" td={"underline"} size="sm" component="button">
+            Create account
+          </Anchor>
+        </Text>
+
+        <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+          <TextInput label="Email" placeholder="you@mantine.dev" required />
+          <PasswordInput
+            label="Password"
+            placeholder="Your password"
+            required
+            mt="md"
+          />
+          <Group justify="space-between" mt="lg">
+            <Anchor component="button" size="sm">
+              Forgot password?
+            </Anchor>
+          </Group>
+          <Button fullWidth mt="xl" color="platinum.8">
+            Sign in
+          </Button>
+        </Paper>
+      </Container>
+    </Stack>
   );
 }

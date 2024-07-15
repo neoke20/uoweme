d1480316db41a5d24a0be6e77ca4e7f20b6cf58a
Kevin Konczak <konczakkevin@gmail.com>
2024-07-15 12:32:54 +0900

    user data are fetched from session

--- app/routes/_app._index.tsx
+++ app/routes/_app._index.tsx
@@ -2,6 +2,7 @@ import type { MetaFunction } from "@remix-run/node";
 import { Box } from "@mantine/core";
 import { useLoaderData } from "@remix-run/react";
 import { PrismaClient } from "@prisma/client";
+import { getSession } from "~/session.server";
 
 const prisma = new PrismaClient();
 
@@ -12,18 +13,20 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-export async function loader() {
+export async function loader({ request }: { request: Request }) {
+  const session = await getSession(request.headers.get("Cookie"));
+  const userId = session?.data.loginUser?.userId;
   const user = await prisma.user.findUnique({
     where: {
-      email: "kevin@k.com",
+      id: userId,
     },
   });
-  return { user };
+  return { user, session };
 }
 
 export default function Index() {
-  const { user } = useLoaderData<typeof loader>();
-  console.log(user);
+  const { user, session } = useLoaderData<typeof loader>();
+  console.log(session);
   return (
     <Box>
       <p>Hi, {user?.name}</p>

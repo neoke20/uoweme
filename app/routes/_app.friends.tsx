731c24ffef68414f610d3dda8e6d0c53bccf9242
Kevin Konczak <konczakkevin@gmail.com>
2024-07-20 12:22:15 +0900

    displaying friends in a card

--- app/routes/_app.friends.tsx
+++ app/routes/_app.friends.tsx
@@ -1,6 +1,9 @@
+import { Alert, Box, Text } from "@mantine/core";
 import { PrismaClient } from "@prisma/client";
 import { useLoaderData } from "@remix-run/react";
 import { requireUser } from "~/auth.server";
+import { FiAlertCircle } from "react-icons/fi";
+import UserCard, { UserProps } from "~/components/UserCard";
 
 const prisma = new PrismaClient();
 
@@ -10,6 +13,15 @@ export async function loader({ request }: { request: Request }) {
     where: {
       userId: sessionUser.userId,
     },
+    include: {
+      receiver: {
+        select: {
+          username: true,
+          surname: true,
+          name: true,
+        },
+      },
+    },
   });
   return { friendships };
 }
@@ -17,9 +29,26 @@ export async function loader({ request }: { request: Request }) {
 export default function Friends() {
   const { friendships } = useLoaderData<typeof loader>();
   console.log(friendships);
+  const icon = <FiAlertCircle />;
   return (
-    <div>
-      <p>Friends</p>
-    </div>
+    <Box>
+      {friendships.length > 0 ? (
+        <Box>
+          <Text>You are friends with:</Text>
+          {friendships.map((friendship) => (
+            <Box key={friendship.id}>
+              <UserCard
+                user={friendship.receiver as UserProps}
+                friendshipDate={friendship.updatedAt}
+              />
+            </Box>
+          ))}
+        </Box>
+      ) : (
+        <Alert color="vermilion.6" icon={icon}>
+          You do not have any friends at the moment
+        </Alert>
+      )}
+    </Box>
   );
 }

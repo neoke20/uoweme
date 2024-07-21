8e0f6c9ca0a9c4d79e7e0b016742182e3e3c8080
Kevin Konczak <konczakkevin@gmail.com>
2024-07-21 11:49:58 +0900

    user can see a list of what is owed to them

---
+++ app/routes/_app.imowed.tsx
@@ -0,0 +1,63 @@
+import { Box, Container, Title } from "@mantine/core";
+import { PrismaClient } from "@prisma/client";
+import { useLoaderData } from "@remix-run/react";
+import { requireUser } from "~/auth.server";
+import DebtCard from "~/components/DebtCard";
+const prisma = new PrismaClient();
+
+export type CreditProps = {
+  id: number;
+  amount: number;
+  creditorId: number;
+  currency: string;
+  title: string;
+  description: string;
+  isPaidInFull: boolean;
+  debtorId: number;
+  debtor: {
+    name: string;
+    surname: string;
+    username: string;
+  };
+  createdAt: string;
+  updatedAt: string;
+};
+
+export async function loader({ request }: { request: Request }) {
+  const sessionUser = await requireUser(request);
+  const creditList = await prisma.debt.findMany({
+    where: {
+      creditorId: sessionUser.userId,
+    },
+    include: {
+      debtor: {
+        select: {
+          name: true,
+          surname: true,
+          username: true,
+        },
+      },
+    },
+  });
+
+  return { creditList };
+}
+
+export default function Imowed() {
+  const { creditList } = useLoaderData<typeof loader>();
+  console.log(creditList);
+  return (
+    <Box>
+      {creditList.length > 0 ? (
+        <Container>
+          <Title order={2} ta="center" my="md">
+            What people owe you
+          </Title>
+          {creditList.map((credit) => (
+            <DebtCard key={credit.id} details={credit as CreditProps} />
+          ))}
+        </Container>
+      ) : null}
+    </Box>
+  );
+}
